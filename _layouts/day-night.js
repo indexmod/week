06a550a3b5e446146c0a5b7290eefdeea4d772aa
Indexmod <23720911+indexmod@users.noreply.github.com>
2024-11-06 11:50:49 +0300

    чиним адрес / динамичный scc / и скорость редиректа

---
+++ _layouts/day-night.js
@@ -0,0 +1,16 @@
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const hour = new Date().getHours();
+        const body = document.body;
+
+        if (hour >= 6 && hour < 9) {
+            body.classList.add("dawn"); // Раннее утро
+        } else if (hour >= 9 && hour < 18) {
+            body.classList.add("day"); // Дневное время
+        } else if (hour >= 18 && hour < 21) {
+            body.classList.add("dusk"); // Вечернее время
+        } else {
+            body.classList.add("night"); // Ночь
+        }
+    });
+</script>
